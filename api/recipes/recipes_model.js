5f8bbb33bc1bf1bd78c3fa45747fe0b6e76aed71
Lukecp <lukecp@pm.me>
2021-04-02 23:05:59 -0400

    completed mvp

--- api/recipes/recipes_model.js
+++ api/recipes/recipes_model.js
@@ -1,5 +1,31 @@
 const db = require('../../data/db-config');
 
-module.exports = {
 
+const getRecipeById = async (id) => {
+	const recipe = await db('recipes as r')
+		.where({ 'r.id': id }).first();
+	
+		const steps = await db('steps as s')
+		.where({'recipe_id': id})
+		.leftJoin('step_ingredients as si', 'si.step_id', 's.id')
+		.leftJoin('ingredients as i', 'si.ingredient_id', 'i.id')
+		.select('s.id as step_id', 'step_number', 'instructions', 'quantity', 'i.id as ingredient_id', 'ingredient_name');
+	
+	let formattedRecipe = {};
+
+	if (recipe) {
+		formattedRecipe = {
+			...recipe,
+			steps: steps
+		}
+	} else {
+		formattedRecipe = [];
+	}
+
+	return formattedRecipe;
+};
+
+
+module.exports = {
+	getRecipeById
 };
--- api/recipes/recipes_router.js
+++ api/recipes/recipes_router.js
@@ -5,7 +5,10 @@ router.get('/:id', (req, res) => {
 	const { id } = req.params;
 	
 	Recipes.getRecipeById(id)
-		.then((data) => res.status(200).json(data))
+		.then((data) => {
+			console.log(data);
+			res.status(200).json(data);
+		})
 		.catch((err) => res.status(500).json({ message: err.message}));
 });
 
